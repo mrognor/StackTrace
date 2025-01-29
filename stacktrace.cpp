78e7e3aee4d7c333ce82383440ab92635d274cde
mrognor <47296449+mrognor@users.noreply.github.com>
2025-01-29 18:13:13 +0300

    Update stacktrace.cpp

--- stacktrace.cpp
+++ stacktrace.cpp
@@ -1,61 +1,55 @@
-#include <cstdint>
+#include <link.h>
 #include <stdio.h>
-#include <execinfo.h>
+#include <dlfcn.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <cxxabi.h>
-#include <sys/wait.h>
-#include <iostream>
+#include <execinfo.h>
+
 #include <sstream>
+#include <iostream>
 
 
 #define TRACE_MAX 1024
 
-void handler()
+
+void print_backtrace()
 {
-    void *array[TRACE_MAX];
+    void* array[TRACE_MAX];
     size_t size;
-    const char msg[] = "failed with a signal\n";
 
     size = backtrace(array, TRACE_MAX);
-    char** stack = backtrace_symbols(array, size);
 
     for (int i = 1; i < size; ++i)
     {
-        std::cout << stack[i] << std::endl;
-        
-        std::string addr;
-        bool f = false;
-        for (char* c = stack[i]; *c != 0; ++c)
-        {
-            if (*c == ')') break;
-            if (f) addr += *c;
-            if (*c == '(') 
-            {
-                ++c;
-                if (*c == '+')
-                    f = true;
-            }
-        }
-        
+        Dl_info info;
+        link_map *extra_info;
+        dladdr1(array[i], &info, reinterpret_cast<void**>(&extra_info), RTLD_DL_LINKMAP);
+        info.dli_fbase = (void *) extra_info->l_addr;
+
+
+	    if (info.dli_sname == NULL)
+		    info.dli_saddr = info.dli_fbase;
+
+        ptrdiff_t offset = static_cast<char*>(array[i]) - static_cast<char*>(info.dli_saddr) - 1;
+
         char buf[BUFSIZ];
         std::size_t readed = readlink("/proc/self/exe", buf, BUFSIZ);
-        std::uint64_t prevAddr = strtoul(addr.c_str(), nullptr, 16) - 1;
-        std::stringstream ss;
-        ss << std::hex << prevAddr;
-        std::string command = "addr2line -e " + std::string(buf) + " " + ss.str();
+        std::stringstream sstream;
+        sstream << std::hex << offset;
+        std::string command =  "addr2line -e " + std::string(buf) + " " + sstream.str();
         system(command.c_str());
-        std::cout << std::endl;
     }
 }
 
 void one()
 {
-    handler();
+    print_backtrace();
 }
 
+// Required addr2line
 // g++ -g main.cpp
 int main(int argc, char **argv) 
 {
     one();
 }
+
